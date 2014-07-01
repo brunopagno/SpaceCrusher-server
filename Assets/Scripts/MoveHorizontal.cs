2dad403f582be0a1e50d934913317e7e0477d35d
Bruno Pagno <brunopagno@gmail.com>
2014-07-01 17:06:43 -0300

    Added project to git

---
+++ Assets/Scripts/MoveHorizontal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveHorizontal : MonoBehaviour {
+
+  public float speed = 0.1f;
+  protected Vector3 movement;
+
+	void Start () {
+        this.movement = new Vector3();
+	}
+	
+	void Update () {
+      if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+          this.transform.position += Vector3.left * speed;
+      }
+      if (Input.GetKeyDown(KeyCode.RightArrow)) {
+		  this.transform.position += Vector3.right * speed;
+      }
+	}
+}
