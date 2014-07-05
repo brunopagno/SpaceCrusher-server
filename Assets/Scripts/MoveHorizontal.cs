a016b5dea32f0d2a6a119a21692e0eecebf857ab
Bruno Pagno <brunopagno@gmail.com>
2014-07-04 21:29:42 -0300

    Made server and client TALK

--- Assets/Scripts/MoveHorizontal.cs
+++ Assets/Scripts/MoveHorizontal.cs
@@ -6,8 +6,7 @@ public class MoveHorizontal : MonoBehaviour {
     public float speed = 2.5f;
     
     void Update() {
-        float transV = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float transH = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        transform.Translate(transH, transV, 0);
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(horizontal, 0, 0);
     }
 }
--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -1,24 +1,40 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Threading;
-using System.Net.Sockets;
-using System.Net;
-using System;
-using System.Text;
 
 public class Server : MonoBehaviour {
 
     public int port = 32154;
     public int maxConnections = 4;
+    public GameObject shipPrefab;
+
+    private const string TYPE_NAME = "IHA-SPG0";
+    private const string GAME_NAME = "SpaceCrusher Game";
+    private string inmessages = "";
 
     void StartServer() {
-        Debug.Log("Server start");
         Network.InitializeServer(maxConnections, port, false);
+        MasterServer.RegisterHost(TYPE_NAME, GAME_NAME);
     }
 
     void StopServer() {
-        Debug.Log("Server stop");
         Network.Disconnect();
+    }
+
+    //void OnPlayerConnected(NetworkPlayer player) {
+    //    this.AddShip();
+    //}
+    //private void AddShip() {
+    //    Network.Instantiate(shipPrefab, new Vector3(333, 444, 0), Quaternion.identity, 0);
+    //}
+
+    [RPC]
+    void RPCOut(string info) {
+        networkView.RPC("RPCIn", RPCMode.Others, info);
+    }
+
+    [RPC]
+    void RPCIn(string info) {
+        Debug.Log("Received message -> " + info);
+        inmessages += info + "\n";
     }
 
     void OnGUI() {
@@ -38,6 +54,10 @@ public class Server : MonoBehaviour {
                 foreach (NetworkPlayer client in Network.connections) {
                     GUILayout.Label("Client " + client);
                 }
+                if (GUILayout.Button("Make magic")) {
+                    RPCOut("isahtoathas");
+                }
+                GUILayout.Label(inmessages);
             }
             if (GUILayout.Button("Stop Server")) {
                 StopServer();
